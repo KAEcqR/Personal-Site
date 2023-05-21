f6a0e729043410713d667e5b94c9fa27d5d5f80b
Kacper P <kacperprochwicz@gmail.com>
2023-05-21 23:41:18 +0200

    added files

---
+++ script.js
@@ -0,0 +1,45 @@
+const parallax_el = document.querySelectorAll(".parallax")
+const main = document.querySelector("main")
+
+let xValue = 0
+let yValue = 0
+
+let rotateDegree = 0
+parallax_el.forEach((el) => {
+    el.style.transform = `translate(-50%, -50%)`
+})
+
+function update(cursorposition) {
+    parallax_el.forEach((el) => {
+    let speedx = el.dataset.speedx
+    let speedy = el.dataset.speedy
+    let speedz = el.dataset.speedz
+
+    let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+    let zValue = (cursorposition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1
+
+    el.style.transform = `
+    translateX(calc(-50% + ${-xValue * speedx}px)) 
+    translateY(calc(-50% + ${yValue * speedy}px))
+    rotateY(${rotateDegree}deg)
+    perspective(2000px)
+    translateZ(calc(${zValue * speedz}px))`
+    })
+}
+
+window.addEventListener("mousemove", (e) => {
+    xValue = e.clientX - window.innerWidth  / 2
+    yValue = e.clientY - window.innerHeight / 2
+
+    rotateDegree = (xValue / (window.innerWidth  / 2) * 10 )
+    
+    update(e.clientX)
+})
+
+if(window.innerWidth >= 725) {
+    main.style.maxHeight = `${window.innerWidth * 0.6}px`
+}else{
+    main.style.maxHeight = `${window.innerWidth * 1.6}px`
+}
+
+
