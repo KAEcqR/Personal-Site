1ef4880338cc6cc3910bec80e8c4914438a5cb60
Kacper P <kacperprochwicz@gmail.com>
2023-05-23 23:42:59 +0200

    Added trailer

---
+++ js/trailer.js
@@ -0,0 +1,28 @@
+const trailer = document.querySelector(".trailer")
+
+const animateTrailer = (e, interacting) => {
+    const x = e.clientX - trailer.offsetWidth / 2;
+    const y = e.clientY - trailer.offsetHeight / 2;
+
+    const keyframes = {
+        transform: `
+            translate(${x}px, ${y}px) 
+            scale(${interacting ? 3:1})`
+    }
+
+    trailer.animate(keyframes, {
+        duration: 500,
+        fill: 'forwards'
+    })
+}
+
+window.onmousemove = e => {
+    const interactible = e.target.closest(".interactible"),
+          interacting = interactible !== null
+
+    animateTrailer(e, interacting)
+
+    if(interacting) {
+        trailer.className
+    }
+}
